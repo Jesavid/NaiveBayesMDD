be8918b8a5a9af90eceabc3ac573716e95f86b4c
Jesus David <jesusdavida.e2089@gmail.com>
2025-02-22 14:30:16 -0700

    ajustar valores para evitar 0

--- naivebayes.m
+++ naivebayes.m
@@ -13,8 +13,11 @@ Benignos=Train((mapBenigno),:);
 numMalignos=size(Malignos,1);
 numBenignos=size(Benignos,1);
 
-% Definir función de probabilidad
+% Definir función de probabilidad que calcula la probabilidad en base a
+% valorColumna para columna
 function [Prob] = Probabilidad(Casos,columna,valorColumna)
+   % Casos: matriz casos, columna: columna a evaluar, valorColumna: valor de la
+   % columna a evaluar
    num=size(Casos,1);
    Mapa=Casos(:,columna)==valorColumna;
    numCritero=sum(Mapa);
@@ -25,3 +28,20 @@ function [Prob] = Probabilidad(Casos,columna,valorColumna)
 totalTrain=size(Train,1);
 probabilidadMaligno=numMalignos/totalTrain;
 probabilidadBenigno=1-probabilidadMaligno;
+
+%Generar un array con lso valores únicos de toda la tabla
+uniqueMaligno(Malignos(:,2:10));
+uniqueBenigno(Benignos(:,2:10));
+
+% Contar instancias para cada caso
+% EVULAR SI ES NECESARIO IMPLEMENTEAR
+
+% Eliminar valores 0
+atributosMalignos=Malignos(:,2:10);
+atributosBenignos=Benignos(:,2:10);
+
+atributosMalignosNormalizado=atributosMalignos+1;
+atributosBenignosNormalizado=atributosBenignos+1;
+
+% Normalización
+
