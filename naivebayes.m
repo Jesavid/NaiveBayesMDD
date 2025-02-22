359b189fd270b550eee20dca6e52560d63c9cd2a
Jesus David <jesusdavida.e2089@gmail.com>
2025-02-22 14:14:55 -0700

    modificación

--- naivebayes.m
+++ naivebayes.m
@@ -13,4 +13,15 @@ Benignos=Train((mapBenigno),:);
 numMalignos=size(Malignos,1);
 numBenignos=size(Benignos,1);
 
+% Definir función de probabilidad
+function [Prob] = Probabilidad(Casos,columna,valorColumna)
+   num=size(Casos,1);
+   Mapa=Casos(:,columna)==valorColumna;
+   numCritero=sum(Mapa);
+   Prob=numCriterio/num;
+ end
 
+% Calcular probabilidad de cada clase
+totalTrain=size(Train,1);
+probabilidadMaligno=numMalignos/totalTrain;
+probabilidadBenigno=1-probabilidadMaligno;
